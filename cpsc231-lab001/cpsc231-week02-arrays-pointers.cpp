777479fc933f5fd8a07eb8bce58111757e4e588e
leer6 <leer6@AS315-D007>
2017-01-18 19:13:12 -0500

    refactored as week02-array-pointers

---
+++ cpsc231-lab001/cpsc231-week02-arrays-pointers.cpp
@@ -0,0 +1,42 @@
+// Author	: Randy Lee
+// Date		: January 18, 2017
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+// const ensures that your array size stays fixed
+const int NUMBER_OF_SCORES = 2; 
+
+// void keyword = no return
+void introduction();
+
+int main()
+{	
+	introduction();
+	int i = 0;
+	int temp = 0;
+	int score[NUMBER_OF_SCORES];
+	score[0] = 100;
+	score[1] = 200;
+
+	cout << endl;
+
+	cout << "element 0 - " << score[0] << endl;
+	cout << "element 1 - " << score[1] << endl;
+
+	temp = score[i];
+	score[i] = score[i + 1];
+	score[i + 1] = temp;
+
+	cout << "temp - " << temp << endl;
+	cout << "element 0 - " << score[0] << endl;
+
+	return 0;
+}
+
+void introduction()
+{
+	cout << "Read in two scores and switch the scores." << endl;
+}
